409df25a22346b9d4a0a9c37e71070450cfca6c6
Bill Morris <bilcon@swbell.net>
2021-12-25 12:31:03 -0600

    added post create route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -63,3 +63,16 @@ router.get("/:id", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post("/", withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_text: req.body.post_text,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
