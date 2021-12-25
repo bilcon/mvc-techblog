fe2ff0c15b670e78732b21c9b6a53c9bb0b8a1ac
Bill Morris <bilcon@swbell.net>
2021-12-25 12:28:56 -0600

    added get find one post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,7 +3,6 @@ const sequelize = require("../../config/connection");
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-// get all users
 router.get("/", withAuth, (req, res) => {
   Post.findAll({
     attributes: ["id", "post_text", "title", "created_at"],
@@ -23,6 +22,42 @@ router.get("/", withAuth, (req, res) => {
     ],
   })
     .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res
+          .status(404)
+          .json({ message: "There was no post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
