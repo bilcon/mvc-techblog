179b62629229befe97e1c1406d4e9e1a15bec072
Bill Morris <bilcon@swbell.net>
2021-12-25 08:48:23 -0600

    added get route to home-routes and added homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,38 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
+// get all posts for homepage
 router.get("/", (req, res) => {
-  res.render("homepage");
-});
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
 
-module.exports = router;
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+        username: req.session.username,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
