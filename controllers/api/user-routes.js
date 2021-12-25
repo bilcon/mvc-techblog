51f1910899e29343f74e440faee9b7ac0099ce6f
Bill Morris <bilcon@swbell.net>
2021-12-25 11:28:05 -0600

    added update put route to user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -100,3 +100,35 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res
+          .status(404)
+          .json({ message: "There was no user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
