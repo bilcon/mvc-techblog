df4957a5b2405a9f09669593b6bd15bf6b7e491f
Bill Morris <bilcon@swbell.net>
2022-01-02 21:10:50 -0600

    adjusted css

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,24 +12,6 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", withAuth, (req, res) => {
-<<<<<<< HEAD
-=======
-
->>>>>>> 734000b04df52f6519adc69a2c8bc55a01f32421
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.session.user_id,
-    post_id: req.body.post_id,
-  })
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-<<<<<<< HEAD
-    });
-=======
-    }); 
->>>>>>> 734000b04df52f6519adc69a2c8bc55a01f32421
   if (req.session) {
     Comment.create({
       comment_text: req.body.comment_text,
